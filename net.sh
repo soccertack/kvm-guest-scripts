3b0349fb7586d5e051a4607f7a24330c0f68a59e
Jintack Lim <jintack@cs.columbia.edu>
2018-02-09 09:45:21 -0600

    Make net.sh flexible to handle various eth names
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- net.sh
+++ net.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-ifconfig eth1 > /dev/null 2>&1
+ETH=`route | grep "10\." | awk '{ print $8 }'`
+
+echo $ETH
+
+ifconfig $ETH > /dev/null 2>&1
 err=$?
 if [[ $err != 0 ]]; then
-	echo "eth1 not found - are you using the right topology?" >&2
+	echo "$ETH not found - are you using the right topology?" >&2
 	exit 1
 fi
 
-IP=`ifconfig eth1 | grep 'inet addr:' | awk '{ print $2 }' | sed 's/.*://'`
-ifconfig eth1 0.0.0.0
+IP=`ifconfig $ETH | grep 'inet addr:' | awk '{ print $2 }' | sed 's/.*://'`
+echo $IP
+ifconfig $ETH 0.0.0.0
 brctl addbr br0
-brctl addif br0 eth1
+brctl addif br0 $ETH
 ifconfig br0 $IP netmask 255.255.255.0
