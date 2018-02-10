33d63424be6e99fa8b1a9549edb1104c76c4ff46
Jintack Lim <jintack@cs.columbia.edu>
2018-02-10 10:00:45 -0600

    Factor out vfio-common
    We will reuse in run-guest-vfio
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- setup-vfio.sh
+++ setup-vfio.sh
@@ -1,17 +1,6 @@
 #/bin/bash
 
-ARCH=`uname -m`
-DEV_ID="1af4 1041"
-
-if [[ "$ARCH" == "x86_64" ]]; then
-	BDF="0000:01:00.0"
-	BDF_S="0000\:01\:00.0"
-	TYPE1_OPTION=""
-else
-	BDF="0000:00:04.0"
-	BDF_S="0000\:00\:04.0"
-	TYPE1_OPTION="allow_unsafe_interrupts=1"
-fi
+source vfio-common.sh
 
 modprobe -v vfio-pci
 modprobe -v vfio_iommu_type1 $TYPE1_OPTION
