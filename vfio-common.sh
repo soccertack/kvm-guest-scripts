84f732b035d6e0b5ba47d55ad896554773f1551f
Jintack Lim <jintack@cs.columbia.edu>
2018-02-11 15:43:11 -0600

    Consider passthrough from L0 on x86
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- vfio-common.sh
+++ vfio-common.sh
@@ -1,13 +1,30 @@
 ARCH=`uname -m`
-DEV_ID="1af4 1041"
 
-if [[ "$ARCH" == "x86_64" ]]; then
-	BDF="0000:01:00.0"
-	BDF_S="0000\:01\:00.0"
-	TYPE1_OPTION=""
+ifconfig | grep -q "10.10.1.2 "
+err=$?
+
+if [[ $err == 0 ]]; then
+#L0 specific settings
+	if [[ "$ARCH" == "x86_64" ]]; then
+		DEV_ID="8086 10fb"
+		BDF="0000:06:00.0"
+		TYPE1_OPTION=""
+		echo "x86 bare-metal passthrough!!"
+	else
+		echo "Passthrough on ARM bare-meatl is not available, yet."
+		exit
+	fi
 else
-	BDF="0000:00:04.0"
-	BDF_S="0000\:00\:04.0"
-	TYPE1_OPTION="allow_unsafe_interrupts=1"
-fi
+	#L1 specific settings
+	DEV_ID="1af4 1041"
 
+	if [[ "$ARCH" == "x86_64" ]]; then
+		BDF="0000:01:00.0"
+		BDF_S="0000\:01\:00.0"
+		TYPE1_OPTION=""
+	else
+		BDF="0000:00:04.0"
+		BDF_S="0000\:00\:04.0"
+		TYPE1_OPTION="allow_unsafe_interrupts=1"
+	fi
+fi
