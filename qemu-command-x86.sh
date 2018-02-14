b52f4097e972a5104ae5218bd2b29dd490b3572a
Jintack Lim <jintack@cs.columbia.edu>
2018-02-14 14:32:51 -0600

    Print out settings on VM boot
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- qemu-command-x86.sh
+++ qemu-command-x86.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "---------- QEMU setup -------------"
+echo "SMP: "$SMP
+echo "MEMSIZE: "$MEMSIZE
+echo "MACHINE: "$MACHINE
+echo "IOMMU: "$IOMMU
+echo "VIRTIO-net: "$VIRTIO_NETDEV
+echo "VFIO_DEV: "$VFIO_DEV
+echo "VFIO_DEV2: "$VFIO_DEV2
+echo "IOMMU_VIRTIO_NETDEV: " $IOMMU_VIRTIO_NETDEV
+echo "IOMMU_VIRTIO_NETDEV2: " $IOMMU_VIRTIO_NETDEV2
+echo "---------- QEMU setup end ---------"
 sudo $QEMU	\
 	-smp $SMP -m $MEMSIZE -M $MACHINE -cpu host	\
 	-drive if=none,file=$FS,id=vda,cache=none,format=raw	\
