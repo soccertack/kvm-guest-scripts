910e7b2324212d0fff4083bccbd2dda9d1d3e650
Jintack Lim <jintack@cs.columbia.edu>
2018-02-13 17:09:21 -0700

    Add APT cluster aware vfio setting
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- vfio-common.sh
+++ vfio-common.sh
@@ -1,13 +1,26 @@
 ARCH=`uname -m`
 
+CLOUD=""
+uname -a | grep -q apt
+err=$?
+if [[ $err == 0 ]]; then
+	CLOUD="APT"
+fi
+
 ifconfig | grep -q "128\."
 err=$?
 
 if [[ $err == 0 ]]; then
 #L0 specific settings
 	if [[ "$ARCH" == "x86_64" ]]; then
-		DEV_ID="8086 10fb"
-		BDF="0000:06:00.0"
+		if [[ "$CLOUD" == "APT" ]]; then
+			echo "x86 APT"
+			DEV_ID="15b3 1003"
+			BDF="0000:08:00.0"
+		else
+			DEV_ID="8086 10fb"
+			BDF="0000:06:00.0"
+		fi
 		TYPE1_OPTION=""
 		echo "x86 bare-metal passthrough!!"
 	else
