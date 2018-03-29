1b2306ad9c0423a72a4c683503f9449d9418f397
Jintack Lim <jintack@cs.columbia.edu>
2018-03-29 12:51:36 -0600

    Set vhost=on by default
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-smmu-v8.sh
+++ run-guest-smmu-v8.sh
@@ -5,7 +5,7 @@ IOMMU=""
 MACHINE="$MACHINE,iommu=smmuv3" 
 
 NETDEV_IOMMU_OPTION="iommu_platform=on,disable-modern=off,disable-legacy=on"
-IOMMU_VIRTIO_NETDEV="-netdev tap,id=net2,vhost=off"
+IOMMU_VIRTIO_NETDEV="-netdev tap,id=net2,vhost=on"
 IOMMU_VIRTIO_NETDEV="$IOMMU_VIRTIO_NETDEV -device virtio-net-pci,netdev=net2,$NETDEV_IOMMU_OPTION"
 
 QEMU="./qemu-smmu-v8/aarch64-softmmu/qemu-system-aarch64"
