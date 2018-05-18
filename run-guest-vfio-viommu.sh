68e0cb5506a768e9660d7ddf47f771e6cc4258a6
Jintack Lim <jintack@cs.columbia.edu>
2018-05-18 13:06:14 -0500

    Use fixes for vIOMMU for L2 direct device assignment
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-vfio-viommu.sh
+++ run-guest-vfio-viommu.sh
@@ -7,6 +7,7 @@ source setup-vfio.sh
 if [[ "$ARCH" == "x86_64" ]]; then
 	IOMMU="-device intel-iommu,intremap=on,caching-mode=on"
 	MACHINE="q35,accel=kvm,kernel-irqchip=split"
+	QEMU="./qemu-vtd-fix/x86_64-softmmu/qemu-system-x86_64"
 	source qemu-command-x86.sh
 else
 	source qemu-command-arm.sh
