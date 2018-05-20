62cf1857a33787d540f5b1e2389dab9a49e8485e
Jintack Lim <jintack@cs.columbia.edu>
2018-05-19 22:18:10 -0500

    Cleanup unused params
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-intel-viommu.sh
+++ run-guest-intel-viommu.sh
@@ -6,18 +6,15 @@ IOMMU="-device intel-iommu,intremap=on,device-iotlb=on"
 
 NETDEV_IOMMU_OPTION="iommu_platform=on,disable-modern=off,disable-legacy=on,ats=on"
 
-IOH="-device ioh3420,id=pcie.1,chassis=1"
-IOMMU_VIRTIO_NETDEV="-netdev tap,id=net1,vhostforce"
-IOMMU_VIRTIO_NETDEV="$IOMMU_VIRTIO_NETDEV -device virtio-net-pci,netdev=net1,bus=pcie.1,$NETDEV_IOMMU_OPTION"
-
-IOH=""
-IOMMU_VIRTIO_NETDEV=""
+#Uncomment below if you want to provide one more virtio device behind virtual IOMMU.
+#IOH="-device ioh3420,id=pcie.1,chassis=1"
+#IOMMU_VIRTIO_NETDEV="-netdev tap,id=net1,vhostforce"
+#IOMMU_VIRTIO_NETDEV="$IOMMU_VIRTIO_NETDEV -device virtio-net-pci,netdev=net1,bus=pcie.1,$NETDEV_IOMMU_OPTION"
+#VIRTIO_NETDEV=""
 
 IOH2="-device ioh3420,id=pcie.1,chassis=2"
 IOMMU_VIRTIO_NETDEV2="-netdev tap,id=net2,vhostforce"
 IOMMU_VIRTIO_NETDEV2="$IOMMU_VIRTIO_NETDEV2 -device virtio-net-pci,netdev=net2,bus=pcie.1,$NETDEV_IOMMU_OPTION"
-
-#VIRTIO_NETDEV=""
 
 MACHINE="q35,accel=kvm,kernel-irqchip=split"
 
