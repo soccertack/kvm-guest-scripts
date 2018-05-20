20cef199e0c85ae8dcf86fda3ca304564e279c48
Jintack Lim <jintack@cs.columbia.edu>
2018-05-19 23:48:58 -0500

    Clean up logic to set BDF and DEV_ID
    NOT TESTED ON ARM!
    
    Make things work in a architecture/device/virt-level agnostic
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- vfio-common.sh
+++ vfio-common.sh
@@ -20,46 +20,39 @@ fi
 ifconfig | grep -q "128\."
 err=$?
 
+# Create VFs in x86 L0
 if [[ $err == 0 ]]; then
-#L0 specific settings
 	if [[ "$ARCH" == "x86_64" ]]; then
 		create_vf
-		if [[ "$CLOUD" == "APT" ]]; then
-			echo "x86 APT"
-			DEV_ID="15b3 1003"
-			BDF="0000:08:00.0"
-		else
-			DEV_ID="8086 10ed"
-			# This is VF BDF
-			BDF="0000:06:10.0"
-		fi
-		TYPE1_OPTION=""
-		echo "x86 bare-metal passthrough!!"
-	else
-		echo "Passthrough on ARM bare-meatl is not available, yet."
-		exit
-	fi
-else
-	#L1 specific settings
-	DEV_ID="1af4 1041"
-
-	if [[ "$ARCH" == "x86_64" ]]; then
-		lspci | grep -q "01:00.0"
-		err=$?
-		if [[ $err == 0 ]]; then
-			BDF="0000:01:00.0"
-		else
-			BDF="0000:00:04.0"
-			DEV_ID="8086 10ed"
-		fi
-		TYPE1_OPTION=""
-		# Enable this option if you want not to use irq-remapping on x86
-		#TYPE1_OPTION="allow_unsafe_interrupts=1"
-	else
-		BDF="0000:00:04.0"
-		BDF_S="0000\:00\:04.0"
-		TYPE1_OPTION="allow_unsafe_interrupts=1"
 	fi
 fi
+
+# TODO: test this on ARM and remove this comment!
+# This physical network device is for Wisc machines.
+BDF_P=`lspci | grep 82599.*Virtual.Function | head -1 | awk '{ print $1 }'`
+BDF_V=`lspci | grep Red.Hat.*1041 | awk '{ print $1 }'`
+
+if [[ "$BDF_P" != "" && "$BDF_V" != "" ]]; then
+	echo "We have VF and virtio-net. Not sure what we want to do"
+	exit
+elif [[ "$BDF_P" == "" && "$BDF_V" == "" ]]; then
+	echo "We have no device to assign to a VM."
+	exit
+elif [[ "$BDF_P" != "" ]]; then
+	BDF=$BDF_P
+else
+	BDF=$BDF_V
+fi
+
+DEV_ID=`lspci -nn | grep $BDF | cut -d[ -f3 | cut -d] -f1 | sed 's/:/ /'`
+
+if [[ "$ARCH" == "aarch64" ]]; then
+	# ARM IOMMU emulation doesn't support interrupt remapping yet
+	# Enable this option on x86 if you want not to use irq-remapping
+	TYPE1_OPTION="allow_unsafe_interrupts=1"
+fi
+
+echo "BDF: "$BDF
+echo "DEV_ID: "$DEV_ID
 
 VFIO_DEV="-device vfio-pci,host=$BDF,id=net2"
