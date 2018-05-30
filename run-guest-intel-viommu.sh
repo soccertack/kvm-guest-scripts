42aa2b6a6dbeff65fad549f8c9ff5e0b081a069b
Jintack Lim <jintack@cs.columbia.edu>
2018-05-30 13:20:11 -0500

    Start with virtio first
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-intel-viommu.sh
+++ run-guest-intel-viommu.sh
@@ -13,7 +13,7 @@ NETDEV_IOMMU_OPTION="iommu_platform=on,disable-modern=off,disable-legacy=on,ats=
 #VIRTIO_NETDEV=""
 
 IOH2="-device ioh3420,id=pcie.1,chassis=2"
-IOMMU_VIRTIO_NETDEV2="-netdev tap,id=net2,vhostforce"
+IOMMU_VIRTIO_NETDEV2="-netdev tap,id=net2,vhost=off"
 IOMMU_VIRTIO_NETDEV2="$IOMMU_VIRTIO_NETDEV2 -device virtio-net-pci,netdev=net2,bus=pcie.1,$NETDEV_IOMMU_OPTION"
 
 MACHINE="q35,accel=kvm,kernel-irqchip=split"
