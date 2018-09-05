ce36b5323d3101202523534052a2a1282ab1c6c1
Jintack Lim <jintack@cs.columbia.edu>
2018-09-05 15:10:58 -0600

    tmp modification for migration test
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-vfio.sh
+++ run-guest-vfio.sh
@@ -9,6 +9,9 @@ source setup-vfio.sh
 VIRTIO_NETDEV=""
 
 if [[ "$ARCH" == "x86_64" ]]; then
+	QEMU=./qemu-vfio-migration/x86_64-softmmu/qemu-system-x86_64
+	CONSOLE="telnet:127.0.0.1:$TELNET_PORT,server,nowait"
+	MON="-monitor stdio"
 	source qemu-command-x86.sh
 else
 	source qemu-command-arm.sh
