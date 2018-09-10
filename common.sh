f49c750f5b91a99e0a7f4aa792ef76d42c1d01fa
Jintack Lim <jintack@cs.columbia.edu>
2018-09-09 18:44:21 -0600

    Never omit quotes around variables
    if you want to use posix-shell
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 ETH=`ifconfig | grep "10\.10\." -B1 | head -n 1 | awk '{ print $1 }'`
 IP=`ifconfig | grep "10\.10\." | awk '{ print $2 }' | cut -d ":" -f2`
 IP_TAIL=`echo $IP | cut -d "." -f4`
-if [ $IP_TAIL -ge 100 ]; then
+if [ "$IP_TAIL" -ge 100 ]; then
 	IS_HOST=0
 else
 	IS_HOST=1
