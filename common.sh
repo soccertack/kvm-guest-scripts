321405928cc7869a4fcb2d720261421a4d7b4da2
Jintack Lim <jintack@cs.columbia.edu>
2018-09-10 21:34:25 -0600

    Consider multiple private IPs
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- common.sh
+++ common.sh
@@ -1,11 +1,16 @@
 ETH=`ifconfig | grep "10\.10\." -B1 | head -n 1 | awk '{ print $1 }'`
+
 IP=`ifconfig | grep "10\.10\." | awk '{ print $2 }' | cut -d ":" -f2`
+# Could be multiple IPs. Take the first one
+IP=`echo $IP | awk '{print $1}'`
 IP_TAIL=`echo $IP | cut -d "." -f4`
 if [ $IP_TAIL -ge 100 ]; then
 	IS_HOST=0
 else
 	IS_HOST=1
 fi
+
+exit
 
 case $ETH in
  *br*)
