84076d9c896026db685761d31ac79180a97da370
Jintack Lim <jintack@cs.columbia.edu>
2019-03-11 09:21:23 -0500

    Move virtio for vp setting to a separate file
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-intel-viommu.sh
+++ run-guest-intel-viommu.sh
@@ -3,21 +3,7 @@
 source run-guest-common.sh
 
 source setup-iommu.sh vp
-
-NETDEV_IOMMU_OPTION="iommu_platform=on,disable-modern=off,disable-legacy=on,ats=on"
-
-#Uncomment below if you want to provide one more virtio device behind virtual IOMMU.
-#IOH="-device ioh3420,id=pcie.1,chassis=1"
-#IOMMU_VIRTIO_NETDEV="-netdev tap,id=net1,vhostforce"
-#IOMMU_VIRTIO_NETDEV="$IOMMU_VIRTIO_NETDEV -device virtio-net-pci,netdev=net1,bus=pcie.1,$NETDEV_IOMMU_OPTION"
-#VIRTIO_NETDEV=""
-
-IOH2="-device ioh3420,id=pcie.1,chassis=2"
-IOMMU_VIRTIO_NETDEV2="-netdev tap,id=net2,vhostforce"
-IOMMU_VIRTIO_NETDEV2="$IOMMU_VIRTIO_NETDEV2 -device virtio-net-pci,netdev=net2,bus=pcie.1,$NETDEV_IOMMU_OPTION"
-
-find_available_mac 3
-IOMMU_VIRTIO_NETDEV2="$IOMMU_VIRTIO_NETDEV2,mac=$MAC"
+source setup-virtio-vp.sh
 
 if [ "$PI" == 1 ]; then
 	QEMU="./qemu-pi/x86_64-softmmu/qemu-system-x86_64"
