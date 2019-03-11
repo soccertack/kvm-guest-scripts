164c5223eeb08249dad5316d3ab8f9531df5a4ae
Jintack Lim <jintack@cs.columbia.edu>
2019-03-11 09:11:40 -0500

    Add setup-iommu script to be used for pt and vp alike
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-vfio-viommu.sh
+++ run-guest-vfio-viommu.sh
@@ -5,8 +5,7 @@ source run-guest-common.sh
 source setup-vfio.sh
 
 if [[ "$ARCH" == "x86_64" ]]; then
-	IOMMU="-device intel-iommu,intremap=on,caching-mode=on"
-	MACHINE="q35,accel=kvm,kernel-irqchip=split"
+	source setup-iommu.sh pt
 	source qemu-command-x86.sh
 else
 	source qemu-command-arm.sh
