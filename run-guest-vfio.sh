edf191b111d0971bf9bb916fe1561e303d81daa1
Jintack Lim <jintack@cs.columbia.edu>
2019-03-11 08:48:06 -0500

    Describe why we don't set virtio device when using an assigned dev
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-guest-vfio.sh
+++ run-guest-vfio.sh
@@ -6,6 +6,8 @@ source setup-vfio.sh
 
 # Do not provide a normal virtio-net device.
 # Doing that resulted in a severe performance drop for vp on x86
+# This is because the VM having both virtio and assigned device will be
+# receiving packets from one of them arbitrarily.
 VIRTIO_NETDEV=""
 
 if [[ "$ARCH" == "x86_64" ]]; then
